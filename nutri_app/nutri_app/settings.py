b96fc16a7c55daeccddaea09c563267e4a061c43
César <cesarlpb@gmail.com>
2025-03-26 23:15:39 +0100

    config de localización

--- nutri_app/nutri_app/settings.py
+++ nutri_app/nutri_app/settings.py
@@ -107,12 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+# Localización:
 
-TIME_ZONE = "UTC"
-
+LANGUAGE_CODE = "es-es"
+TIME_ZONE = "Europe/Madrid"
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
